2b2377fe90fffe632df431697a55dac2e17b3e2a
Ellis Velandia <eyis619@gmail.com>
2024-12-21 20:31:59 -0500

    model ai

---
+++ app.py
@@ -0,0 +1,9 @@
+from google import genai
+
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash-exp", contents="Is gemini Flash expensive?"
+)
+
+print(response.text)
